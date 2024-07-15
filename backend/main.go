e1d1b86ee73ee69dd2de7ea1f4f94c0a40e18798
10Harshul <105785175+10Harshul@users.noreply.github.com>
2024-07-15 19:44:28 +0000

    Create /items

--- backend/main.go
+++ backend/main.go
@@ -5,10 +5,16 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+type Item struct{
+	ID int = 'json:"id"'
+	NAME string = 'json:"name"'
+}
+
 func main() {
 	router := gin.Default()
 	router.GET("/", greet)
 	router.HEAD("/healthcheck", healthcheck)
+	router.GET("/items", getItems)
 
 	router.Run()
 }
@@ -21,4 +27,19 @@ func healthcheck(c *gin.Context) {
 	c.JSON(http.StatusOK, gin.H{
 		"status": "ok",
 	})
+
 }
+
+func getItems(c *gin.Context){
+	items := []Item{
+		{ID: 1, NAME: "Galactic Goggles"},
+		{ID: 2, Name: "Meteor Muffins"},
+		{ID: 3, Name: "Alien Antenna Kit"},
+        {ID: 4, Name: "Starlight Lantern"},
+        {ID: 5, Name: "Quantum Quill"},
+	}
+
+	c.JSON(http.StatusOK, items)
+	
+}
+
