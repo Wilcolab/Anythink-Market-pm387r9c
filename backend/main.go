27054ece5f95658c50e03e3d8495d2e6733fdbea
10Harshul <105785175+10Harshul@users.noreply.github.com>
2024-07-15 20:23:45 +0000

    Create /items

--- backend/main.go
+++ backend/main.go
@@ -1,45 +1,41 @@
 package main
 
 import (
-	"net/http"
-	"github.com/gin-gonic/gin"
+    "net/http"
+    "github.com/gin-gonic/gin"
 )
 
-type Item struct{
-	ID int  `json:"id"`
-	NAME string  `json:"name"`
+type Item struct {
+    ID   int    `json:"id"`
+    Name string `json:"name"`
 }
 
 func main() {
-	router := gin.Default()
-	router.GET("/", greet)
-	router.HEAD("/healthcheck", healthcheck)
-	router.GET("/items", getItems) //Added this line
+    router := gin.Default()
+    router.GET("/", greet)
+    router.HEAD("/healthcheck", healthcheck)
+    router.GET("/items", getItems) // Add this line
 
-	router.Run()
+    router.Run()
 }
 
 func greet(c *gin.Context) {
-	c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
+    c.IndentedJSON(http.StatusOK, "Welcome, Go navigator, to the Anythink cosmic catalog.")
 }
 
 func healthcheck(c *gin.Context) {
-	c.JSON(http.StatusOK, gin.H{
-		"status": "ok",
-	})
-
+    c.JSON(http.StatusOK, gin.H{
+        "status": "ok",
+    })
 }
 
-func getItems(c *gin.Context){
-	items := []Item{
-		{ID: 1, Name: "Galactic Goggles"},
-		{ID: 2, Name: "Meteor Muffins"},
-		{ID: 3, Name: "Alien Antenna Kit"},
+func getItems(c *gin.Context) {
+    items := []Item{
+        {ID: 1, Name: "Galactic Goggles"},
+        {ID: 2, Name: "Meteor Muffins"},
+        {ID: 3, Name: "Alien Antenna Kit"},
         {ID: 4, Name: "Starlight Lantern"},
         {ID: 5, Name: "Quantum Quill"},
-	}
-
-	c.JSON(http.StatusOK, items)
-	
+    }
+    c.JSON(http.StatusOK, items)
 }
-
