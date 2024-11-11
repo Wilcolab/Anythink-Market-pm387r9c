6563a9a9cec5e2286eec3ae363dc779303b00934
WilcoApp <hello@wilco.gg>
2024-11-11 16:17:17 +0000

    Initial commit

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -49,44 +49,25 @@ const LoggedInView = (props) => {
   );
 };
 
-const Header = (props) => {
+class Header extends React.Component {
+  render() {
+    return (
+      <nav
+        className="navbar navbar-expand-md navbar-dark"
+        style={{ padding: "0.5rem 2rem" }}
+      >
+        <Link to="/" className="navbar-brand">
+          <img alt="logo" src={logo} />
+        </Link>
 
-  const {currentUser} = props
-  return (
-    <nav className="navbar navbar-expand-md navbar-dark" style={{ padding: "0.5rem 2rem" }}>
-      <Link to="/" className="navbar-brand">
-        <img alt="logo" src={logo} />
-      </Link>
-
-      {currentUser ? (
-        <LoggedInView currentUser={currentUser} />
-      ) : (
-        <LoggedOutView currentUser={currentUser} />
-      )}
-    </nav>
-
-  )
+        {this.props.currentUser ? (
+          <LoggedInView currentUser={this.props.currentUser} />
+        ) : (
+          <LoggedOutView currentUser={this.props.currentUser} />
+        )}
+      </nav>
+    );
+  }
 }
-
-// class Header extends React.Component {
-//   render() {
-//     return (
-//       <nav
-//         className="navbar navbar-expand-md navbar-dark"
-//         style={{ padding: "0.5rem 2rem" }}
-//       >
-//         <Link to="/" className="navbar-brand">
-//           <img alt="logo" src={logo} />
-//         </Link>
-
-//         {this.props.currentUser ? (
-//           <LoggedInView currentUser={this.props.currentUser} />
-//         ) : (
-//           <LoggedOutView currentUser={this.props.currentUser} />
-//         )}
-//       </nav>
-//     );
-//   }
-// }
 
 export default Header;
